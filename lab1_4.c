be081a3193eea0f7d508d48097a72f0d805816f2
R2BT <theeraphat.a@ku.th>
2021-02-01 18:57:09 +0700

    x2

--- lab1_4.c
+++ lab1_4.c
@@ -1,22 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,i=0;
-    scanf("%d",&n);
-    int a[n];
-    for(i;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    int max=a[0],loca=0;
-    for(i=1;i<n;i++)
-    {
-        if(a[i]>max)
-        {
-            max=a[i];
-            loca=i;
-        }
-    }
-    printf("%d %d",loca+1,max);
+    long long x1,x2,s;
+    scanf("%lld %lld",&x1,&s);
+    x2=(2*s)-x1;
+    printf("%lld",x2);
+
+    return 0;
 }
 
